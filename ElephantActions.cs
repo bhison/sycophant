c1b94a39b63dfc53b8e99252c23a4e8468d58df6
Tim Bhison <catbat04@gmail.com>
2024-01-28 10:33:22 +0000

    stuff

--- ElephantActions.cs
+++ ElephantActions.cs
@@ -18,12 +18,12 @@ public class ElephantActions
 
     public void AskForClothes(ClothesType clothesType, Describer describer)
     {
+        var guidance = Guidance.YouWantClothes(clothesType, describer);
         DialogueParameters dialogueParameters = new DialogueParameters
         {
             Context =
-                new string[] { "You want some " + clothesType.ToString() + " that are " + describer.ToString() },
-            Guidance = "Ask the assistant if they have any " + clothesType.ToString() + " that are " +
-                       describer.ToString(),
+                new string[] { "You want some more clothes" },
+            Guidance = guidance,
             LookingForALaugh = false
         };
         SaySomething.Instance.Speak(dialogueParameters);
@@ -31,6 +31,9 @@ public class ElephantActions
 
     public void ReceiveItem(ClothesType recClothesType, Describer recDescriber)
     {
+        if (ElephantController.Instance.wantedClothesType == null) return;
+        if (ElephantController.Instance.wantedDescriber == null) return;
+        
         var wantedClothes = (ClothesType)ElephantController.Instance.wantedClothesType;
         var wantedDesc = (Describer)ElephantController.Instance.wantedDescriber;
         var contexts = new List<string>();
@@ -60,6 +63,45 @@ public class ElephantActions
         {
             Context = new string[] { "the assistant gave you what you wanted" },
             Guidance = "Thank the assistant and give them a tip of £" + (amount),
+            LookingForALaugh = false
+        };
+        SaySomething.Instance.Speak(dialogueParameters);
+    }
+
+    public void TellFunnyAnecdote()
+    {
+        DialogueParameters dialogueParameters = new DialogueParameters
+        {
+            Context = new string[] { "Tell a funny anecdote" },
+            Guidance = "Tell the assistant a funny anecdote",
+            LookingForALaugh = true
+        };
+        SaySomething.Instance.Speak(dialogueParameters);
+    }
+
+    public void OffendedAtLackOfLaughter()
+    {
+        DialogueParameters dialogueParameters = new DialogueParameters
+        {
+            Context = new string[] { "You just said something funny" },
+            Guidance = "Express your disappointment at the lack of laughter at your wonderful humor",
+            LookingForALaugh = false
+        };
+        SaySomething.Instance.Speak(dialogueParameters);
+    }
+
+    public void ExpressRapport()
+    {
+        var rapport = GameManager.Instance.RapportPercent;
+        var context = rapport > 0.7f
+            ? "you are enjoying the assistant's company"
+            : rapport > 0.3f
+                ? "you are tolerating the assistant."
+                : "you don't like the assistant.";
+        DialogueParameters dialogueParameters = new DialogueParameters
+        {
+            Context = new string[] { context },
+            Guidance = Guidance.ExpressYourself(),
             LookingForALaugh = false
         };
         SaySomething.Instance.Speak(dialogueParameters);
